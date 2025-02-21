a08c8ed5025e611413fb05a6a5793f62489a5d32
marouane-sadoune <marouanesadoune@gmail.com>
2025-02-21 21:20:48 +0100

    update DB.php

--- system/database/DB.php
+++ system/database/DB.php
@@ -1,19 +1,4 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP 4.3.2 or newer
- *
- * @package		CodeIgniter
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2008 - 2009, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
- * @filesource
- */
-
-// ------------------------------------------------------------------------
 
 /**
  * Initialize the database
